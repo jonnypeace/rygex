590a7adb15aff9a5315fc7de7459f9d6d4447273
jonnypeace <royalpython2@hotmail.com>
2024-06-15 16:43:18 +0100

    updated test script

--- test-script.sh
+++ test-script.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-printf '\n%s\n' '001: -s and -e with -O on big file'
-if time ./pygrep.py -s 'SRC=' 1 -e ' DST' 1 -O -f ufw.test1 | wc -l; then
+printf '\n%s\n' '001: String Search using -s and -e with -O on ufw.test1'
+if time ./pygrep.py -s 'SRC=' 1 -e ' DST' 1 -O -Sc -f ufw.test1; then
 	printf '%s\n\n' '001 complete'
 fi
 
-printf '\n%s\n' '002: -p -m 12 on big file'
-if time ./pygrep.py -p '\w+\s+DST=(123.12.123.12)\s+\w+' 1 -m 12 -f ufw.test1 | wc -l; then
+printf '\n%s\n' '002: -p "\w+\s+DST=(123.12.123.12)\s+\w+" -m 12 multithreaded on ufw.test1'
+if time ./pygrep.py -p '\w+\s+DST=(123.12.123.12)\s+\w+' 1 -m 12 -Sc -f ufw.test1; then
 	printf '%s\n\n' '002 complete'
+fi
+
+
+printf '\n%s\n' '003: -p "\sDST=([\d\.]+)\s" single threaded on ufw.test1'
+if time ./pygrep.py -p '\sDST=([\d\.]+)\s' '1' -Sc -f ufw.test1; then
+	printf '%s\n\n' '003 complete'
+fi
+
+printf '\n%s\n' '004: -p "\sDST=([\d\.]+)\s" multi (12) threaded on ufw.test1'
+if time ./pygrep.py -p '\sDST=([\d\.]+)\s' '1' -m12 -Sc -f ufw.test1; then
+	printf '%s\n\n' '003 complete'
 fi
