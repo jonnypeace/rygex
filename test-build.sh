c1bdd4a6d2c9b221af3977f547adb3d3466ab278
jonnypeace <royalpython2@hotmail.com>
2025-05-14 21:48:26 +0100

    need it to glob the release number

--- test-build.sh
+++ test-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 maturin build --release
-pip install target/wheels/rygex-0.1.0-cp313-cp313-manylinux_2_34_x86_64.whl --force-reinstall
+pip install target/wheels/rygex-*-cp313-cp313-manylinux_2_34_x86_64.whl --force-reinstall
